7555acd962f6802a2f6bca48e77028ca730b2b89
Sasa Ethernal <sasa.matic@ethernal.tech>
2023-10-05 11:12:34 +0200

    Added pages

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -19,3 +19,59 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error 2", 500)
 	}
 }
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./static/views/login.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error 1", 500)
+		return
+	}
+	ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error 2", 500)
+	}
+}
+
+func Transactions(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./static/views/transactions.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error 1", 500)
+		return
+	}
+	ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error 2", 500)
+	}	
+}
+
+func AddEditTransaction(w http.ResponseWriter, r *http.Request) {	
+	ts, err := template.ParseFiles("./static/views/transaction_add_edit.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error 1", 500)
+		return
+	}
+	ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error 2", 500)
+	}
+}
+
+func TransactionHistory(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./static/views/transaction_history.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error 1", 500)
+		return
+	}
+	ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error 2", 500)
+	}
+}
--- server.go
+++ server.go
@@ -8,7 +8,19 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", handlers.Index)
+	mux.HandleFunc("/login", handlers.Login)
+	mux.HandleFunc("/transactions", handlers.Transactions)
+	mux.HandleFunc("/transactions/add", handlers.AddEditTransaction)
+	mux.HandleFunc("/transactions/edit", handlers.AddEditTransaction)
+	mux.HandleFunc("/transactions/history", handlers.TransactionHistory)
+
+	mux.HandleFunc("/api/transactions/add", handlers.ApiTransactionAdd)
+	mux.HandleFunc("/api/transactions/edit", handlers.ApiTransactionEdit)
+	mux.HandleFunc("/api/transactions/addPolicy", handlers.ApiTransactionAddPolicy)
+	mux.HandleFunc("/api/transactions/cancel", handlers.ApiTransactionCancel)
+
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
