115aa4f08827a5e28a9aa17f96669fd5ed658638
grujicf998 <grujicf998@gmail.com>
2023-10-30 17:44:21 +0100

    Clear main function

--- server.go
+++ server.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -18,88 +16,8 @@ func main() {
 		Handler: app.routes(),
 	}
 
-	// log.Println(app.db.GetTransactionTypes())
-
-	// am := app.db.CheckCFM(1, 1)
-	// log.Println(am)
-	// b := app.db.GetBanks()
-	// log.Println(b)
-
-	// r := app.db.InsertTransaction(DB.Transaction{
-	// 	OriginatorBank:  1,
-	// 	BeneficiaryBank: 2,
-	// 	Sender:          1,
-	// 	Receiver:        2,
-	// 	Currency:        "$$",
-	// 	Amount:          54001,
-	// 	TypeId:          1,
-	// })
-
-	// fmt.Println("Inserted tx with ID: ", r)
-
 	fmt.Println("Start server...")
 	err := server.ListenAndServe()
 	app.db.Close()
 	log.Fatal(err)
-
-	url := "http://localhost:9090/api/start-server"
-
-	// Create an HTTP client.
-	client := &http.Client{}
-
-	// Create a JSON payload (in this example).
-	jsonPayload := []byte(`{"tx_id": "3", "policy_id": "1"}`)
-
-	// Create an HTTP request with the payload.
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
-	if err != nil {
-		panic(err)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Connection", "close")
-	// Set the Content-Type header to specify the data
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	// Handle the response as needed.
-	// (e.g., read the response body, check status codes, etc.)
-	respbody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(respbody)
-
-	//--------------------------------------------------------------------------------------
-	url = "http://localhost:9090/api/start-client"
-
-	// Create a JSON payload (in this example).
-	jsonPayload = []byte(`{"tx_id": "3", "receiver": "ReceiverOfFunds", "to": "0.0.0.0:10501"}`)
-
-	// Create an HTTP request with the payload.
-	req, err = http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
-	if err != nil {
-		panic(err)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Connection", "close")
-	// Set the Content-Type header to specify the data
-	resp, err = client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	// Handle the response as needed.
-	// (e.g., read the response body, check status codes, etc.)
-	respbody, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(respbody)
-
 }
