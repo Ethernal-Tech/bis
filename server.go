cc1f7ab2aabccc32d721dc5ccdcde2f373fdea2b
milos-ethernal <milos.maksimovic@ethernal.tech>
2024-05-13 13:46:14 +0200

    Add sanction-lists initialization

--- server.go
+++ server.go
@@ -4,9 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
+
+func init() {
+	// Initialize sanction list folder
+	if _, err := os.Stat("./sanction-lists"); err != nil {
+		if os.IsNotExist(err) {
+			err = os.Mkdir("sanction-lists", 0777)
+			if err != nil {
+				panic(fmt.Sprint("error while creating sanction-lists folder %w", err.Error()))
+			}
+		} else {
+			panic(fmt.Sprint("error while searching for sanction-lists folder %w", err.Error()))
+		}
+	}
+
+	// Initialize sanction list
+	if _, err := os.Stat("./sanction-lists/UN_List.csv"); err != nil {
+		if os.IsNotExist(err) {
+			if _, err := getNewestSanctionsList(); err != nil {
+				panic(fmt.Sprint("error while downloading latest sanction list %w", err.Error()))
+			}
+		} else {
+			panic(fmt.Sprint("error while searching for sanction list %w", err.Error()))
+		}
+	}
+}
 
 func main() {
 	err := godotenv.Load(".env")
