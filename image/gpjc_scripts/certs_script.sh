51b8e8d5b44e1feff852c7f5bf7bdad1b6e28224
milos-ethernal <milos.maksimovic@ethernal.tech>
2024-05-27 15:07:07 +0200

    Add GPJC API to docker image

---
+++ image/gpjc_scripts/certs_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if DNS_NAME environment variable is set
+if [ -z "$DNS_NAME" ]; then
+  echo "DNS_NAME environment variable is not set."
+  exit 1
+fi
+
+# Generate valid Server Key/Cert
+openssl genrsa -passout pass:1234 -des3 -out server.key 4096
+openssl req -passin pass:1234 -new -key server.key -out server.csr -subj  "/C=SP/ST=Spain/L=Valdepenias/O=Test/OU=Server/CN=$DNS_NAME"
+openssl x509 -req -passin pass:1234 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
+​
+# Remove passphrase from the Server Key
+openssl rsa -passin pass:1234 -in server.key -out server.key
+​
+# Generate valid Client Key/Cert
+openssl genrsa -passout pass:1234 -des3 -out client.key 4096
+openssl req -passin pass:1234 -new -key client.key -out client.csr -subj  "/C=SP/ST=Spain/L=Valdepenias/O=Test/OU=Client/CN=client"
+openssl x509 -passin pass:1234 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
+​
+# Remove passphrase from Client Key
+openssl rsa -passin pass:1234 -in client.key -out client.key
