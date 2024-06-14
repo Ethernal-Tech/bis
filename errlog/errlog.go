b9405ab8e2ab350e6fe6b43b5780bd1c3983b2ee
filip <grujicf998@gmail.com>
2024-06-15 00:01:47 +0200

    Add flags to the errlog print function

--- errlog/errlog.go
+++ errlog/errlog.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func Println(err error) {
+func Println(err error, flags ...bool) {
 	pc, file, line, _ := runtime.Caller(1)
 	funcName := runtime.FuncForPC(pc).Name()
 	funcNameEls := strings.Split(funcName, ".")
@@ -19,5 +19,10 @@ func Println(err error) {
 
 	dateTime := strings.Split(time.Now().String(), ".")[0]
 	file = "./app" + strings.Split(file, "/app")[1]
+
+	if flags != nil && flags[0] {
+		line -= 2
+	}
+
 	fmt.Printf("\033[31m%v ERROR: \033[0m%v:%v - %v: \033[32m%v\033[0m\n", dateTime, file, line, funcName, err)
 }
