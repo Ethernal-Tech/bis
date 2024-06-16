a892b17a8107cb541e5a6673b61523aae2b1eeb9
filip <grujicf998@gmail.com>
2024-06-16 13:57:07 +0200

    Update p2p server multiplexer

--- app/P2P/server/server.go
+++ app/P2P/server/server.go
@@ -4,7 +4,9 @@ import (
 	"bisgo/app/P2P/core"
 	"bisgo/app/P2P/handler"
 	"bisgo/app/P2P/messages"
+	"bisgo/errlog"
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
 )
@@ -31,16 +33,20 @@ func GetP2PServer() *P2PServer {
 
 func (s *P2PServer) Mux() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// returning a 200 OK response regardless of message correctness, handling of invalid messages is internal
+		w.WriteHeader(http.StatusOK)
+
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
-			http.Error(w, "Unable to read request body", http.StatusBadRequest)
+			errlog.Println(err)
 			return
 		}
 
 		var message messages.P2PServerMessasge
 		err = json.Unmarshal(body, &message)
 		if err != nil {
-			http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+			errlog.Println(err)
 			return
 		}
 
@@ -58,10 +64,7 @@ func (s *P2PServer) Mux() http.Handler {
 		case "cfm-result-originator":
 			go s.CFMResultOriginator(message.MessageID, message.Payload)
 		default:
-			http.Error(w, "Invalid method", http.StatusBadRequest)
-			return
+			errlog.Println(errors.New("invalid p2p method received"))
 		}
-
-		w.WriteHeader(http.StatusOK)
 	})
 }
