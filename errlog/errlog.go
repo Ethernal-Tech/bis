cbba133f35c32888d1090460bc77a0b603dd5c69
filip <grujicf998@gmail.com>
2024-07-15 22:54:24 +0200

    Fix error log function

--- errlog/errlog.go
+++ errlog/errlog.go
@@ -18,11 +18,11 @@ func Println(err error, flags ...bool) {
 	}
 
 	dateTime := strings.Split(time.Now().String(), ".")[0]
-	file = "./app" + strings.Split(file, "/app")[1]
+	file = "./app" + strings.Split(file, "/app")[2]
 
 	if flags != nil && flags[0] {
 		line -= 2
 	}
 
-	fmt.Printf("\033[31m%v ERROR: \033[0m%v:%v - %v: \033[32m%v\033[0m\n", dateTime, file, line, funcName, err)
+	fmt.Printf("\033[31m%v ERROR: \033[0m%v:%v [%v]: \033[32m%v\033[0m\n", dateTime, file, line, funcName, err)
 }
