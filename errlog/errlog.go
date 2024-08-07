ba83eb7fb1e1b9a2bf9ade0b803e2bdbcfab3ce3
filip <grujicf998@gmail.com>
2024-08-08 00:50:56 +0200

    Fix errlog function

--- errlog/errlog.go
+++ errlog/errlog.go
@@ -7,10 +7,11 @@ import (
 	"time"
 )
 
-func Println(err error, flags ...bool) {
+func Println(err error) {
 	pc, file, line, _ := runtime.Caller(1)
 	funcName := runtime.FuncForPC(pc).Name()
 	funcNameEls := strings.Split(funcName, ".")
+	fmt.Println(funcName, file, funcNameEls)
 	if len(funcNameEls) == 2 {
 		funcName = funcNameEls[1] + "()"
 	} else {
@@ -18,10 +19,13 @@ func Println(err error, flags ...bool) {
 	}
 
 	dateTime := strings.Split(time.Now().String(), ".")[0]
-	file = "./app" + strings.Split(file, "/app")[2]
 
-	if flags != nil && flags[0] {
-		line -= 2
+	// (if) for docker container
+	// (else) for general purposes
+	if strings.Count(file, "app") == 2 {
+		file = "./app" + strings.Split(file, "/app")[2]
+	} else {
+		file = "./app" + strings.Split(file, "/app")[1]
 	}
 
 	fmt.Printf("\033[31m%v ERROR: \033[0m%v:%v [%v]: \033[32m%v\033[0m\n", dateTime, file, line, funcName, err)
